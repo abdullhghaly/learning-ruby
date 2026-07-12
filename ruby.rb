c70a654399241608f0d55935cc55c05a941a00c5
abdullhghaly <abdullhghaly@gmail.com>
2026-07-12 17:39:54 +0300

    modify string in ruby

--- ruby.rb
+++ ruby.rb
@@ -1,5 +1,10 @@
-name = "abdullh"
-age = 21
-ismale = true
-have_a_job = false
-flaws = nil
+name = " Abdullh ghaly     "
+puts name.upcase()
+puts name.downcase()
+puts name.strip()
+puts name.length()
+puts name.include? " gh"
+puts name.include? " ghs"
+puts name[7]
+puts name[1,13]
+puts name.index("y")
