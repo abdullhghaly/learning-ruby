0a1742f06e64805d80830928249482ffef833d1a
abdullhghaly <abdullhghaly@gmail.com>
2026-07-14 15:47:20 +0300

    using user inputs in ruby

--- ruby.rb
+++ ruby.rb
@@ -1,15 +1,6 @@
-puts 5 + 9
-puts 5 - 9
-puts 5 * 9
-puts 5 / 9.0
-puts 2**5
-puts 10 % 3
-num = 20
-puts ("my fav num is " + num.to_s)
-puts ( 50 + num)
-puts ( -20.abs )
-puts ( 20.6648.round)
-puts ( 20.6648.ceil)
-puts ( 20.6648.floor)
-puts ( Math.sqrt(36))
-puts ( Math.log(1))
+print "hello what`s your name : "
+name = gets
+puts ("welcome "+name+", you are cool")
+print "hello what`s your name : "
+name = gets.chomp()
+puts ("welcome "+name+", you are cool")
