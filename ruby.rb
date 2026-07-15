0081642a92cacc75b1466e89005087fcd24009b8
abdullhghaly <abdullhghaly@gmail.com>
2026-07-15 13:55:53 +0300

    hashes in ruby

--- ruby.rb
+++ ruby.rb
@@ -1,18 +1,10 @@
-fruits = Array["apple","orange","mango"]
-puts "Array index 0"
-puts fruits[0]
-puts "Array index -2"
-puts fruits[-2]
-fruits = Array.new
-fruits[0]="apple"
-fruits[2]="mango"
-puts "Array with index 1 empty"
-puts fruits
-fruits = Array["apple","orange","mango"]
-puts "is mango in Array , is mangos in Array"
-puts fruits.include? "mango"
-puts fruits.include? "mangos"
-puts "reverse Array"
-puts fruits.reverse
-puts "sort Array"
-puts fruits.sort
+states={
+  :pannsylvania => "PA",
+  "New york" => "NY",
+  "Oregon" => "OR",
+  1 => "one"
+}
+
+puts states["Oregon"]
+puts states[:pannsylvania]
+puts states[1]
