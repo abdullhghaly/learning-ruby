d3968362ff1f74172fe6b4143c4439b63d3da914
abdullhghaly <abdullhghaly@gmail.com>
2026-07-16 02:18:17 +0300

    build a simple calc in ruby

--- ruby.rb
+++ ruby.rb
@@ -1,39 +1,18 @@
-ismale = true
+puts "Enter First number: "
+num1=gets.chomp().to_f
+puts "Enter an Operator: "
+op=gets.chomp()
+puts "Enter Second number: "
+num2=gets.chomp().to_f
 
-if ismale
-  puts "you are man"
+if op == "+"
+  puts (num1 + num2)
+elsif op == "-"
+  puts (num1 - num2)
+elsif op == "/"
+  puts (num1 / num2)
+elsif op == "*"
+  puts (num1 * num2)
+else 
+  puts "Invalid Operator"
 end
-
-ismale = false
-
-if ismale
-else
-  puts "you are woman"
-end
-
-ismale=true
-istall=true
-if ismale and istall
-  puts "you are tall man"
-elsif ismale and !istall
-  puts "you are short man"
-elsif !ismale and istall
-  puts "you are tall woman"
-elsif !ismale or !istall
-  puts "you are short woman"
-else
-  puts "you either not male or tall or both"
-end
-
-
-def max(num1,num2,num3)
-  if num1>=num1 and num2>=num3
-    return num1
-  elsif num2>=num1 and num2>=num3
-    return num2
-  elsif num3>=num1 and num3>=num2
-    return num3
-  end
-end
-
-puts max(100,20,3)
