eae16fb5e4f988957c25642cc13c2db1cdfd66e4
abdullhghaly <abdullhghaly@gmail.com>
2026-07-15 16:36:07 +0300

    mathods in ruby

--- ruby.rb
+++ ruby.rb
@@ -1,10 +1,7 @@
-states={
-  :pannsylvania => "PA",
-  "New york" => "NY",
-  "Oregon" => "OR",
-  1 => "one"
-}
+def sayhi(name=nil,age=nil)
+  puts ("hello "+name+" you are "+age.to_s)
+end
 
-puts states["Oregon"]
-puts states[:pannsylvania]
-puts states[1]
+puts "top"
+sayhi(name=gets.chomp() ,age=gets.chomp())
+puts "bottom"
