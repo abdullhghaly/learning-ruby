a6a591c82ed05c277de8641f26ed310c188d6e46
abdullhghaly <abdullhghaly@gmail.com>
2026-07-15 22:10:42 +0300

    using if statement with methods that compare three nums and print the bigger number

--- ruby.rb
+++ ruby.rb
@@ -1,8 +1,39 @@
-def cube(num)
-  return num*num*num , 70
+ismale = true
 
-  puts "hello"
+if ismale
+  puts "you are man"
 end
 
-puts cube(3)[1]
-puts cube(3)[0]
+ismale = false
+
+if ismale
+else
+  puts "you are woman"
+end
+
+ismale=true
+istall=true
+if ismale and istall
+  puts "you are tall man"
+elsif ismale and !istall
+  puts "you are short man"
+elsif !ismale and istall
+  puts "you are tall woman"
+elsif !ismale or !istall
+  puts "you are short woman"
+else
+  puts "you either not male or tall or both"
+end
+
+
+def max(num1,num2,num3)
+  if num1>=num1 and num2>=num3
+    return num1
+  elsif num2>=num1 and num2>=num3
+    return num2
+  elsif num3>=num1 and num3>=num2
+    return num3
+  end
+end
+
+puts max(100,20,3)
