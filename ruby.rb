dc5613b497fdef567b2fb6fd507feea25b408d13
abdullhghaly <abdullhghaly@gmail.com>
2026-07-17 22:35:43 +0300

    build a guessing game let you have three tries to won the game

--- ruby.rb
+++ ruby.rb
@@ -1,5 +1,22 @@
-index=1
-while index <= 8
-  puts index
-  index += 1
+secret_word = "what"
+guess=""
+guess_count=0
+guess_limit=3
+out_og_guess= false
+puts "let`s play a guess game"
+while guess != secret_word and !out_og_guess
+  if guess_count < guess_limit
+    puts "Enter guess: "
+    guess = gets.chomp()
+    guess_count += 1
+  else
+    out_og_guess = true
+  end
+end
+
+if out_og_guess
+  puts  "you lose"
+else 
+  puts "you won!!"
+  
 end
