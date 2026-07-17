9abf5ce26920f1fb12d629532da276aa56cfd73e
abdullhghaly <abdullhghaly@gmail.com>
2026-07-17 22:05:25 +0300

    ruby while loop

--- ruby.rb
+++ ruby.rb
@@ -1,25 +1,5 @@
-def get_day_name (day)
-  day_name = ""
-  case day
-  when "mon"
-    day_name = "monday"
-  when "tue"
-    day_name = "tuesday"
-  when "wed"
-    day_name = "wednsday"
-  when "thu"
-    day_name = "thursday"
-  when "fri"
-    day_name = "friday"
-  when "sat"
-    day_name = "saturday"
-  when "sun"
-    day_name = "sunday"
-  else
-    day_name="Invalid abbreviation"
-  end
-
-  return day_name
+index=1
+while index <= 8
+  puts index
+  index += 1
 end
-
-puts get_day_name(gets.chomp)
